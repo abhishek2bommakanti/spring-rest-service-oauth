8bd79ec21a50edc985325f9e25e7de51c73d444c
Abhishek Bommakanti <abhishek@guestplate.com>
2015-10-11 14:11:52 -0700

    Added service to create users

--- src/main/java/hello/OAuth2ServerConfiguration.java
+++ src/main/java/hello/OAuth2ServerConfiguration.java
@@ -21,6 +21,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -57,6 +58,7 @@ public class OAuth2ServerConfiguration {
 			// @formatter:off
 			http
 				.authorizeRequests()
+					.antMatchers(HttpMethod.POST, "/users").anonymous()
 					.antMatchers("/users").hasRole("ADMIN")
 					.antMatchers("/greeting").authenticated();
 			// @formatter:on
--- src/main/java/hello/UserController.java
+++ src/main/java/hello/UserController.java
@@ -20,10 +20,15 @@ import hello.data.User;
 import hello.data.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
 	private final UserRepository userRepository;
@@ -32,10 +37,16 @@ public class UserController {
 	public UserController(UserRepository userRepository) {
 		this.userRepository = userRepository;
 	}
-
-	@RequestMapping("/users")
+	
+	@RequestMapping(method=RequestMethod.GET)
 	public Iterable<User> getUsers() {
 		return userRepository.findAll();
 	}
 
+	@RequestMapping(method=RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	public User createUser(@RequestBody User user) {
+		User createdUser = this.userRepository.save(user);
+		return createdUser;
+	}
 }
