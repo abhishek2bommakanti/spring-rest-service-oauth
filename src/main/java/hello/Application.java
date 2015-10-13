eecc928fd07e8db670431fcccf18c77f31f265b8
Abhishek Bommakanti <abhishek@guestplate.com>
2015-10-12 22:53:55 -0700

    Further changes

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -17,9 +17,15 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@ComponentScan
+@Configuration
+@EnableAutoConfiguration
 public class Application {
 
 	public static void main(String[] args) {
