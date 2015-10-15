1f37e5a1c763d79e8c564e07d5c5106bffecbbaf
Abhishek Bommakanti <abhishek@guestplate.com>
2015-10-14 23:07:56 -0700

    Spring Social dependencies

---
+++ src/main/java/hello/DataConfig.java
@@ -0,0 +1,24 @@
+package hello;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class DataConfig {
+	
+	@Bean
+	public DataSource guestPlateDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/trial");
+		dataSource.setUsername("dbadmin");
+		dataSource.setPassword("password");
+		return dataSource;
+	}
+
+}
